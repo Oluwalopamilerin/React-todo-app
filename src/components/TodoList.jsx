9657ed05fdbf61f9eb4f398b6f43cffa46d11bac
Pamilerin Oyewole <pamilerinoyewole06@gmail.com>
2024-03-02 20:21:50 +0100

    corrected the move down button function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -32,11 +32,11 @@ const TodoList=() => {
   }
   
   const moveTaskDown=(index) => {
-     if (index < tasks.length - 1){
-       const updatedTasks=[...tasks];
-       [updatedTasks[index], updatedTasks[index+1]] = 
-       [updatedTasks[[index+1], updatedTasks[index]]]
-       setTasks(updatedTasks);
+    //  if (index < tasks.length - 1){
+    //    const updatedTasks=[...tasks];
+    //    [updatedTasks[index], updatedTasks[index+1]] = 
+    //    [updatedTasks[[index+1], updatedTasks[index]]]
+    //    setTasks(updatedTasks);
      }
   }
   
